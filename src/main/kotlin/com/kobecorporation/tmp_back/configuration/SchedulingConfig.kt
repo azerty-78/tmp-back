3280274fd9a6c2bc2dbf7e6c558ef913c6017ebb
Ben Djibril <konedjibril2004@gmail.com>
2026-01-14 15:49:44 +0100

    Add scheduling and WebFlux configuration classes to enable scheduling and customize argument resolvers for reactive web applications

--- src/main/kotlin/com/kobecorporation/tmp_back/configuration/SchedulingConfig.kt
+++ src/main/kotlin/com/kobecorporation/tmp_back/configuration/SchedulingConfig.kt
@@ -1,4 +1,8 @@
 package com.kobecorporation.tmp_back.configuration
 
-class SchedulingConfig {
-}
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig
--- src/main/kotlin/com/kobecorporation/tmp_back/configuration/WebFluxConfig.kt
+++ src/main/kotlin/com/kobecorporation/tmp_back/configuration/WebFluxConfig.kt
@@ -1,4 +1,16 @@
 package com.kobecorporation.tmp_back.configuration
 
-class WebFluxConfig {
+//import com.kobecorporation.tmp_back.configuration.security.jwt.OptionalAuthenticationPrincipalResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+
+@Configuration
+class WebFluxConfig : WebFluxConfigurer {
+
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.addCustomResolver(
+            //OptionalAuthenticationPrincipalResolver()
+        )
+    }
 }
